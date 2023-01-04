3abc1c37e78ffed089708e01304fa6e52df686a8
Romil003 <romilghadge33@gmail.com>
2023-01-04 18:32:15 +0530

    Added : Logger to log messages

--- src/main/java/com/bridgelabz/springconceptsapplication/SpringConceptsApplication.java
+++ src/main/java/com/bridgelabz/springconceptsapplication/SpringConceptsApplication.java
@@ -2,17 +2,21 @@ package com.bridgelabz.springconceptsapplication;
 
 import com.bridgelabz.springconceptsapplication.Component.DemoBean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
+    public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Spring Concept Demo");
+        logger.debug("Welcome to Spring Concept Demo");
         ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
-        System.out.println("Checking context " + context.getBean(DemoBean.class));
+        logger.debug("Checking context: {} " , context.getBean(DemoBean.class));
+        logger.debug("\n*** Example using @Autowire annotation on property ***");
     }
 
 }
